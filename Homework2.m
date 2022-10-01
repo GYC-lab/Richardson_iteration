82feb24c880168176ebf92d5827451ac6c85016c
GYC <yuchen.ge@stu.pku.edu.cn>
2022-10-01 18:09:43 +0800

    add 2-norm of errors & residuals

--- Homework2.m
+++ Homework2.m
@@ -13,16 +13,13 @@ b=[-3.0;-9.0];
 lambda=eig(A);                          % get eigenvalues
 max_eig=max(lambda);                    % get the max eigenvalue
 u= A\b;                                 % the exact solution
-
 alpha_0=[0.06,0.1,0.2,0.22,0.24,0.4];   % relaxation factor
 I = eye(2);                             % the 2¡Á2 unit matrix
-for i=1:1:size(alpha_0,2)
-    x1=[0;0];
-    alpha=alpha_0(i);
-    B=I-alpha*A;
-    C=alpha*b;
-    for k=1:500
-        x2=B*x1+C;
-        x1=x2;
-    end
+
+num_steps = 10;
+figure
+for i=1:size(alpha_0,2)
+    [v,e,r]=Richardson_iteration(A,b,alpha_0(i),num_steps);
+%     plot(e);
+%     hold on
 end
--- Richardson_iteration.m
+++ Richardson_iteration.m
@@ -1,4 +1,4 @@
-function [v,e,r] = Richardson_iteration(A,b,alpha,num_steps)
+function [v,e_norm,r_norm] = Richardson_iteration(A,b,alpha,num_steps)
 %RICHARDSON_ITERATION - solve a linear system like Ax = b
 %   input : A           - matrix
 %           b           - vector
@@ -11,20 +11,24 @@ function [v,e,r] = Richardson_iteration(A,b,alpha,num_steps)
     % initialization
     u = A\b;                % the exact solution
     I = eye(2);             % the 2¡Á2 unit matrix
-    x1=[0;0];
+    x1=[0.0;0.0];
     
     % Richardson_iteration
     B=I-alpha*A;
     C=alpha*b;
-    for k=1:num_steps
+    e_norm=zeros(1,num_steps);
+    r_norm=zeros(1,num_steps);
+    for i=1:num_steps
+        % iteration
         x2=B*x1+C;
         x1=x2;
+        % error and residual
+        v=x2;
+        e=v-u;      % errors
+        r=b-A*v;    % residuals    
+        e_norm(i)=norm(e); % 2-norm of errors
+        r_norm(i)=norm(r); % 2-norm of residuals
     end
-    
-    % error and residual
-    v=x2;
-    e=v-u;      % error
-    r=b-A*v;    % residual
     
 end
 
